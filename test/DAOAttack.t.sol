d245ce9b09bf09a482ac478913c76c3979af9c64
Tiago de Paula <tiagodepalves@gmail.com>
2025-12-04 06:55:47 -0300

    refactor: split DAO tests from attack

--- test/DAOAttack.t.sol
+++ test/DAOAttack.t.sol
@@ -5,15 +5,30 @@ import { Test } from "forge-std/Test.sol";
 
 import { DAO } from "../src/DAO.sol";
 
-contract DAOAttackTest is Test {
-    address immutable ATTACKER = makeAddr("attacker");
+contract DAOTest is Test {
+    uint256 constant CROWDSALE_BLOCK_NUMBER = 1_599_200;
+    uint256 constant DAO_INITIAL_BALANCE = 11_725_826.068_359_058_772_488_243 ether;
+
+    function setUp() public virtual {
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), CROWDSALE_BLOCK_NUMBER);
+    }
+
+    function test_initialState() external view {
+        vm.assertEq(address(DAO).balance, DAO_INITIAL_BALANCE);
+    }
+
     address constant BITTREX = 0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98;
 
-    uint256 constant BLOCK_NUMBER = 1_599_200;
-    uint256 constant INITIAL_BALANCE = 11_725_826.068_359_058_772_488_243 ether;
+    function test_balanceOf() external view {
+        vm.assertEq(DAO.balanceOf(BITTREX), 9.99 ether);
+    }
+}
 
-    function setUp() public {
-        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), BLOCK_NUMBER);
+contract DAOAttackTest is DAOTest {
+    address immutable ATTACKER = makeAddr("attacker");
+
+    function setUp() public override {
+        super.setUp();
 
         gainTrust();
     }
@@ -26,10 +41,6 @@ contract DAOAttackTest is Test {
         vm.prank(BITTREX);
         bool success = DAO.transfer(ATTACKER, 1 ether);
         require(success, "Donation failed");
-    }
-
-    function test_initialState() external view {
-        vm.assertEq(address(DAO).balance, INITIAL_BALANCE);
     }
 
     function test_attackerIsHolder() external view {
